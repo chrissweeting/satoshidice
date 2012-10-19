6f5b46b32d9d812a181bc8d2a3aa655f6a295ced
Stephen Gornick <sgornick@digicoast.com>
2012-10-18 22:59:43 -0700

    Major redesign of site.

--- satoshidice.com/full.php
+++ satoshidice.com/full.php
@@ -19,21 +19,21 @@
 </script>
 </HEAD>
 <BODY>
-<H1>Transaction ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e</H1>
-<H2> ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e:1</H2>
+<H1>Transaction 02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4</H1>
+<H2> 02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4:0</H2>
 <p class='data'>
-<li class='data'>Processed time: 2012-07-18 05:01:38</li>
-<li class='data'>Key date: 2012-07-18</li>
-<li class='data'>Bet: lessthan 32768</li>
-<li class='data'>Bet TX: <a href='http://blockchain.info/search?search=ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e'>ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e</a></li>
-<li class='data'>Payment TX: <a href='http://blockchain.info/search?search=4e5b87f86c3218a74e0273f8a2f0ef02f9ac01dfea9554ac88bf77631b2c61ec'>4e5b87f86c3218a74e0273f8a2f0ef02f9ac01dfea9554ac88bf77631b2c61ec</a></li>
-<li class='data'>Payment TX Status: CONFIRMED</li>
-<li class='data'>Bet Amount: 0.05000000</li>
+<li class='data'>Processed time: 2012-10-19 04:03:13</li>
+<li class='data'>Key date: 2012-10-19</li>
+<li class='data'>Bet: lessthan 16000</li>
+<li class='data'>Bet TX: <a href='http://blockchain.info/search?search=02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4'>02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4</a></li>
+<li class='data'>Payment TX: <a href='http://blockchain.info/search?search=31cf98c26e2670a3c86ad76812b448209d195c08b3c486fa05cff4b71f91cdb7'>31cf98c26e2670a3c86ad76812b448209d195c08b3c486fa05cff4b71f91cdb7</a></li>
+<li class='data'>Payment TX Status: UNKNOWN</li>
+<li class='data'>Bet Amount: 0.00100000</li>
 <li class='data'>Outcome: LOSE</li>
 <li class='data'>Payment: 0.00000001</li>
-<li class='data'>Address: 1w2pxFjrQ5x5idtL5N7fkGuWXoNGRcqdS <a href='http://blockchain.info/address/1w2pxFjrQ5x5idtL5N7fkGuWXoNGRcqdS'><img height='16' width='16' src='https://s3-eu-west-1.amazonaws.com/satoshidice/blockchain-cube39.png'></a></li>
-<li class='data'>Lucky number: 55158</li>
+<li class='data'>Address: 1EYP37DvfV6irqC45JzcFDospP3R42s74a <a href='http://blockchain.info/address/1EYP37DvfV6irqC45JzcFDospP3R42s74a'><img height='16' width='16' src='https://s3-eu-west-1.amazonaws.com/satoshidice/blockchain-cube39.png'></a></li>
+<li class='data'>Lucky number: 47683</li>
 </p>
-<H3>Validation</H3><H4>Secret hashes</H4><p>Download and verify <a href='https://s3-eu-west-1.amazonaws.com/satoshidice/hash.keys'>hash.keys</a><br><p>For more details on the secret keys see <a href='secrets.php'>secrets</a><H4>Using correct secret key</H4><p>Verify that the secret used hashes to the hash listed for for 2012-07-18 in <a href='https://s3-eu-west-1.amazonaws.com/satoshidice/hash.keys'>hash.keys</a><br> sha256(hidden) -> d9451383ab51a2bb0cf8dee1578b87f533de7c7a8486ec3b66dad6ad3ff19025 </p><H4>Secret and Transaction Hash</H4><p>Verify that the hmac sha512 with secret and transaction_id hash to the bet hash<br>hmac_sha512(hidden,ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e) -> d776e93e660460ea098713814fb9d4c82ecd565bed7d444d098eb172f52e5f3f  <a href='hmac512.php?secret=hidden&data=ffa2bae0c76cbc0a5756ccf815ecb6c528a3fe5022cd9cbb8dc51fc38e4d7f5e'>hmac512</a></p><H4>Lucky Number</H4><p>Verify that the first bytes of the bet hash above is the lucky number<br>d776e93e660460ea098713814fb9d4c82ecd565bed7d444d098eb172f52e5f3f -> d776 -> 55158</p>
+<H3>Validation</H3><H4>Secret hashes</H4><p>Download and verify <a href='https://s3-eu-west-1.amazonaws.com/satoshidice/hash.keys'>hash.keys</a><br><p>For more details on the secret keys see <a href='secrets.php'>secrets</a><H4>Using correct secret key</H4><p>Verify that the secret used hashes to the hash listed for for 2012-10-19 in <a href='https://s3-eu-west-1.amazonaws.com/satoshidice/hash.keys'>hash.keys</a><br> sha256(hidden) -> d2a7c6d99e61849d5fd2bbffe72231f2338cf46b62e98f687c290fc1b8251a44 </p><H4>Secret and Transaction Hash</H4><p>Verify that the hmac sha512 with secret and transaction_id hash to the bet hash<br>hmac_sha512(hidden,02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4) -> ba435b51c2caf6cff57cfed6535cde4998c008c6cccc11970a0efacd9be39017  <a href='hmac512.php?secret=hidden&data=02cd9c12b5e560fedb2087334713b92b4e2bd53b53a1565627bf69bf2a6b67a4'>hmac512</a></p><H4>Lucky Number</H4><p>Verify that the first bytes of the bet hash above is the lucky number<br>ba435b51c2caf6cff57cfed6535cde4998c008c6cccc11970a0efacd9be39017 -> ba43 -> 47683</p>
 </BODY>
 </HTML>
---
+++ satoshidice.com/js/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+	
+	//Dropdown
+	if ($('.dropdown').length) {	
+		
+		$('.dropdown').sSelect();
+		
+	}
+	
+	//Tabs
+	if ($('.tabs').length) {
+
+		$('.tabs').tabs( 'div.panes > div' );
+	}
+	
+	//Dropbox
+	if ($('.dropbox').length)
+		{	
+		$('.dropbox').hide();
+		$('.how').show();
+		$('.how').click(function()
+			{
+			$('.verificationbox').hide();	
+			$('.advancedbox').hide();	
+			$('.howbox').slideToggle();	
+			});	
+		$('.closebox').click(function()
+			{
+			$('.howbox').hide();
+			});
+			
+		$('.advanced').show();
+		$('.advanced').click(function()
+			{
+			$('.verificationbox').hide();	
+			$('.howbox').hide();
+			$('.warningbox').hide();
+			$('.advancedbox').slideToggle();	
+			});	
+		$('.closebox').click(function()
+			{
+			$('.advancedbox').hide();	
+			});	
+
+		$('.verification').show();
+		$('.verification').click(function()
+			{
+			$('.howbox').hide();
+			$('.advancedbox').hide();
+			$('.warningbox').hide();
+			$('.verificationbox').slideToggle();	
+			});	
+		$('.closebox').click(function()
+			{
+			$('.verificationbox').hide();	
+			});	
+
+			
+		$('.warning').show();
+		$('.warning').click(function()
+			{
+			$('.howbox').hide();
+			$('.advancedbox').hide();
+			$('.verificationbox').hide();
+			$('.warningbox').slideToggle();	
+			});	
+		$('.closebox').click(function()
+			{
+			$('.warningbox').hide();	
+			});	
+		}
+		
+	//Tooltip
+	$('a[title]').qtip({
+		style: {
+			tip: {
+				corner: false
+			}
+		}
+	})
+
+	//Equal Heights
+	$(".equal").equalHeights();	
+});
